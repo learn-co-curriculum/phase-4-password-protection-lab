cb4667a04477415dd59e7c7f135728e5b39e02ba
ihollander <ianholla@gmail.com>
2021-05-18 09:23:16 -0400

    Added solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
 end
